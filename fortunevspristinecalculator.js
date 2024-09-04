e120e2190bdbb10e8bd94e481bcc8360cc8347e6
Victiniiiii <victinii494@gmail.com>
2024-09-04 21:26:34 +0300

    updated fortunevspristine

--- fortunevspristinecalculator.js
+++ fortunevspristinecalculator.js
@@ -3,8 +3,8 @@ function calculateFortunevsPristine(x) {
     let pristine = document.getElementById(`FvPinput${x+2}`).value;
 
     if ((fortune > 0) && (pristine > 0) && (fortune < 10000) && (pristine < 100)) {
-        let expecteddrop = (( 1 + pristine * 0.79) * (1 + fortune / 100) * 3).toFixed(0)
-        document.getElementById(`FvPoutput${x}`).innerText = expecteddrop+" Rough Gemstones";
+        let expecteddrop = ((( 1 + pristine * 0.79) * (1 + fortune / 100) * 3)/80).toFixed(2)
+        document.getElementById(`FvPoutput${x}`).innerText = "The average drop for a gemstone mined is "+expecteddrop+" Flawed Gemstones.";
     } else {
         document.getElementById(`FvPoutput${x}`).innerText = "Please double-check your values.";
     }    
