5e36fe4a5acab0f0130f8ad125042bd3befbb2e6
Victiniiiii <victinii494@gmail.com>
2024-09-04 12:28:13 +0300

    effectivehp done

--- effectivehp.js
+++ effectivehp.js
@@ -4,8 +4,8 @@ function effectivehpcalculate() {
 
     if ((hp > 0) && (def > 0) && (hp < 100000000000000000) & (def < 100000000000000000)) {
         let effectivehp = hp * ( 1 + ( def / 100));
-        document.getElementById("output").innerText = effectivehp.toFixed(0) + " Effective HP";
+        document.getElementById("effectivehpoutput").innerText = effectivehp.toFixed(0) + " Effective HP";
     } else {
-        document.getElementById("output").innerText = "Please double-check your values.";
+        document.getElementById("effectivehpoutput").innerText = "Please double-check your values.";
     }    
 }
