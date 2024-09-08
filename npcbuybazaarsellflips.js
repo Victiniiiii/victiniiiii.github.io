07be003a9c0dd4159073f8263835ebfa4b1b2904
Victiniiiii <victinii494@gmail.com>
2024-09-08 14:24:31 +0300

    beta version

--- npcbuybazaarsellflips.js
+++ npcbuybazaarsellflips.js
@@ -1,112 +1,193 @@
 let toggleStates;
 let itemsarray = [
 	{
-		name: "Enchanted Quartz",
-		id: "ENCHANTED_QUARTZ",
-	},
-	{
 		name: "Packed Ice",
 		id: "PACKED_ICE",
+        amount: "640",
+        source: "Builder",
+        npc: "9"
 	},
 	{
 		name: "Sand",
 		id: "SAND",
+        amount: "1280",
+        source: "Farm Merchant, Builder",
+        npc: "3" // mean of all sellers
 	},
 	{
 		name: "Experience Bottle",
 		id: "EXP_BOTTLE",
+        amount: "640",
+        source: "Librarian",
+        npc: "30"        
 	},
+    {
+        name: "Grand Experience Bottle",
+        id: "GRAND_EXP_BOTTLE",
+        amount: "640",
+        source: "Mage Emissary",
+        npc: "5000"
+    },
 	{
 		name: "Flint",
 		id: "FLINT",
+        amount: "640",
+        source: "Pat",
+        npc: "6"
 	},
 	{
 		name: "Iron Ingot",
 		id: "IRON_INGOT",
+        amount: "1280",
+        source: "Mine Merchant, Iron Forger",
+        npc: "5.25" // mean of all sellers
 	},
 	{
 		name: "Slimeball",
 		id: "SLIME_BALL",
+        amount: "640",
+        source: "Adventurer",
+        npc: "14"
 	},
 	{
 		name: "Oak Wood",
 		id: "LOG",
+        amount: "640",
+        source: "Lumber Merchant",
+        npc: "5"
 	},
 	{
 		name: "Raw Fish",
 		id: "RAW_FISH",
+        amount: "640",
+        source: "Fish Merchant",
+        npc: "20"
 	},
 	{
 		name: "Jungle Wood",
 		id: "LOG:3",
+        amount: "640",
+        source: "Lumber Merchant",
+        npc: "5"
 	},
 	{
 		name: "Gunpowder",
 		id: "SULPHUR",
+        amount: "640",
+        source: "Adventurer",
+        npc: "10"
 	},
 	{
 		name: "Pufferfish",
 		id: "RAW_FISH:3",
+        amount: "640",
+        source: "Fish Merchant",
+        npc: "40"
 	},
 	{
 		name: "Ice",
 		id: "ICE",
+        amount: "640",
+        source: "Merchant",
+        npc: "1"
 	},
 	{
 		name: "String",
 		id: "STRING",
+        amount: "640",
+        source: "Adventurer",
+        npc: "10"
 	},
 	{
 		name: "Acacia Wood",
 		id: "LOG:2",
+        amount: "640",
+        source: "Lumber Merchant",
+        npc: "5"
 	},
 	{
 		name: "Magma Cream",
 		id: "MAGMA_CREAM",
+        amount: "640",
+        source: "Alchemist",
+        npc: "20"
 	},
 	{
 		name: "Coal",
 		id: "COAL",
+        amount: "640",
+        source: "Mine Merchant",
+        npc: "4"
 	},
 	{
 		name: "End Stone",
 		id: "ENDER_STONE",
+        amount: "640",
+        source: "Pearl Dealer",
+        npc: "10"
 	},
 	{
 		name: "Spruce Wood",
 		id: "LOG:1",
+        amount: "640",
+        source: "Lumber Merchant",
+        npc: "5"
 	},
 	{
 		name: "Cobblestone",
 		id: "COBBLESTONE",
+        amount: "1280",
+        source: "Mine Merchant, Builder",
+        npc: "2.5" // mean of all sellers
 	},
 	{
 		name: "Wheat",
 		id: "WHEAT",
+        amount: "640",
+        source: "Farm Merchant",
+        npc: "10"
 	},
 	{
 		name: "Sugar Cane",
 		id: "SUGAR_CANE",
+        amount: "640",
+        source: "Farm Merchant",
+        npc: "10"
 	},
 	{
 		name: "Carrot",
 		id: "CARROT_ITEM",
+        amount: "640",
+        source: "Farm Merchant",
+        npc: "10"
 	},
 	{
 		name: "Pumpkin",
 		id: "PUMPKIN",
+        amount: "640",
+        source: "Farm Merchant",
+        npc: "25"
 	},
 	{
 		name: "Potato",
 		id: "POTATO_ITEM",
+        amount: "640",
+        source: "Farm Merchant",
+        npc: "10"
 	},
 	{
 		name: "Melon",
 		id: "MELON",
+        amount: "640",
+        source: "Farm Merchant",
+        npc: "4"
 	},
 	{
 		name: "Cocoa Beans",
 		id: "INK_SACK:3",
+        amount: "640",
+        source: "Farm Merchant",
+        npc: "5"
 	},
 ];
 
@@ -195,7 +276,22 @@ document.addEventListener("DOMContentLoaded", function () {
 		document.getElementById("settings").classList.toggle("expanded");
 	});
 
+    const test = document.getElementById("test22222");
+    for (let i = 0; i < itemsarray.length; i++) {
+        const testtest = document.createElement("div");
+		testtest.className = "NbBsBox";
+        testtest.id = `NbBsBox${i}`
+
+        const test1 = document.createElement("img");
+        test1.src = "static/images/Blaze_Rod.png"
+        
+        test.appendChild(test1);
+        test.appendChild(testtest);        
+        // MINI FOTO
+    }
+
 	bazaarconnect();
+
 });
 
 async function bazaarconnect() {
@@ -204,6 +300,19 @@ async function bazaarconnect() {
 
 	for (let i = 0; i < itemsarray.length; i++) {
 		document.getElementById(`prices${itemsarray[i].id}`).innerHTML = format(data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"].toFixed(0)) + " coins";
+        document.getElementById(`NbBsBox${i}`).innerHTML = itemsarray[i].name + "<br>";
+        document.getElementById(`NbBsBox${i}`).innerHTML += "Profit: ";
+        document.getElementById(`NbBsBox${i}`).innerHTML += format((itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc)).toFixed(0)) + " coins<br>"
+        if (itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc) > 100000) {
+            document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#0f6319"
+        } else if (itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc) > 10000){
+            document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#81cc8a"
+        } else if (itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc) > 0){
+            document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#d5db23"
+        } else {
+            document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#db2323"
+        }
+        document.getElementById(`NbBsBox${i}`).innerHTML += `Source: ${itemsarray[i].source}`;
 	}
 }
 
@@ -211,18 +320,4 @@ function format(x) {
 	return Number.parseFloat(x)
 		.toString()
 		.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function test() {
-    const test = document.getElementById("test");
-    for (let i = 0; i < itemsarray.length; i++) {
-        const testtest = document.createElement("div");
-		testtest.className = "NbBsBox";
-        test.appendChild(testtest);
-        // 1- NAME
-        // 2- Total profit by selling
-        // 3- Acquiring place
-        // MINI FOTO
-        // eğer profit x değerin üstündeyse style: green değilse red
-    }
 }
