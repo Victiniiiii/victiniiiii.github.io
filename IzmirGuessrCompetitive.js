9ad452d1ddf4273fd6e57aa1782b23b18d4b405a
Victiniiiii <victinii494@gmail.com>
2024-10-11 20:53:26 +0300

    second try on dark mode, more line weight at rounds 1-4

--- IzmirGuessrCompetitive.js
+++ IzmirGuessrCompetitive.js
@@ -176,114 +176,92 @@ function getRandomLocation() {
 	return { lat, lng };
 }
 
+const darkModeStyleinGoogleMaps = [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#263c3f" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#6b9a76" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#212a37" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#746855" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#1f2835" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#f3d19c" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#2f3948" }],
+    },
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+]
+
 function loadGoogleMapsAPI(callback) {
 	const script = document.createElement("script");
 	script.src = `https://maps.googleapis.com/maps/api/js?key=${theKey}&libraries=places&v=weekly&callback=${callback}`;
 	document.body.appendChild(script);
 }
-
-const darkModeStyle = [
-    {
-        elementType: 'geometry',
-        stylers: [{ color: '#212121' }]
-    },
-    {
-        elementType: 'labels.icon',
-        stylers: [{ visibility: 'off' }]
-    },
-    {
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#757575' }]
-    },
-    {
-        elementType: 'labels.text.stroke',
-        stylers: [{ color: '#212121' }]
-    },
-    {
-        featureType: 'administrative',
-        elementType: 'geometry',
-        stylers: [{ color: '#757575' }]
-    },
-    {
-        featureType: 'administrative.country',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#9e9e9e' }]
-    },
-    {
-        featureType: 'administrative.land_parcel',
-        stylers: [{ visibility: 'off' }]
-    },
-    {
-        featureType: 'administrative.locality',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#bdbdbd' }]
-    },
-    {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#757575' }]
-    },
-    {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{ color: '#181818' }]
-    },
-    {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#616161' }]
-    },
-    {
-        featureType: 'poi.park',
-        elementType: 'labels.text.stroke',
-        stylers: [{ color: '#1b1b1b' }]
-    },
-    {
-        featureType: 'road',
-        elementType: 'geometry.fill',
-        stylers: [{ color: '#2c2c2c' }]
-    },
-    {
-        featureType: 'road',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#8a8a8a' }]
-    },
-    {
-        featureType: 'road.arterial',
-        elementType: 'geometry',
-        stylers: [{ color: '#373737' }]
-    },
-    {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{ color: '#3c3c3c' }]
-    },
-    {
-        featureType: 'road.highway.controlled_access',
-        elementType: 'geometry',
-        stylers: [{ color: '#4e4e4e' }]
-    },
-    {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#616161' }]
-    },
-    {
-        featureType: 'transit',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#757575' }]
-    },
-    {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{ color: '#000000' }]
-    },
-    {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#3d3d3d' }]
-    }
-];
 
 function initMap() {
 	function initializeMapWithRandomLocation() {
@@ -314,6 +292,7 @@ function initMap() {
 			zoom: 14,
 			streetViewControl: false,
 			addressControl: false,
+            styles: darkModeStyleinGoogleMaps,
 		});
 
 		const streetViewService = new google.maps.StreetViewService();
@@ -325,6 +304,7 @@ function initMap() {
 					zoom: 1,
 					addressControl: false,
 					streetViewControl: false,
+                    styles: darkModeStyleinGoogleMaps,
 				});
 
 				gamemap.setStreetView(panorama);
@@ -336,7 +316,7 @@ function initMap() {
 					streetViewControl: false,
 					mapTypeControl: false,
 					clickableIcons: false,
-                    styles: darkModeStyle
+                    styles: darkModeStyleinGoogleMaps,         
 				});
 
 				google.maps.event.addListener(minimap, "click", function (event) {
@@ -411,6 +391,7 @@ function returnToStart() {
 		zoom: 14,
 		streetViewControl: false,
 		addressControl: false,
+        styles: darkModeStyleinGoogleMaps,
 	});
 
 	const streetViewService = new google.maps.StreetViewService();
@@ -422,6 +403,7 @@ function returnToStart() {
 				zoom: 1,
 				streetViewControl: false,
 				addressControl: false,
+                styles: darkModeStyleinGoogleMaps,
 			});
 
 			gamemap.setStreetView(panorama);
@@ -472,6 +454,7 @@ function displayResults(distance, points) {
         streetViewControl: false,
         mapTypeControl: false,
         clickableIcons: false,
+        styles: darkModeStyleinGoogleMaps,
     });
 
     guessedLocationMarker.setMap(resultMap);
@@ -493,7 +476,7 @@ function displayResults(distance, points) {
             geodesic: true,
             strokeColor: "#FF0000", // Red
             strokeOpacity: 1.0,
-            strokeWeight: 2,
+            strokeWeight: 4,
         });
     
         line.setMap(resultMap);	
