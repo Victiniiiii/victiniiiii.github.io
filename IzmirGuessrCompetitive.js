4e32a2cf713581ea5d47d0bd36edbebd97319e9b
Victiniiiii <victinii494@gmail.com>
2024-10-11 20:44:53 +0300

    trying to make minimap dark mode

--- IzmirGuessrCompetitive.js
+++ IzmirGuessrCompetitive.js
@@ -182,6 +182,109 @@ function loadGoogleMapsAPI(callback) {
 	document.body.appendChild(script);
 }
 
+const darkModeStyle = [
+    {
+        elementType: 'geometry',
+        stylers: [{ color: '#212121' }]
+    },
+    {
+        elementType: 'labels.icon',
+        stylers: [{ visibility: 'off' }]
+    },
+    {
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#757575' }]
+    },
+    {
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#212121' }]
+    },
+    {
+        featureType: 'administrative',
+        elementType: 'geometry',
+        stylers: [{ color: '#757575' }]
+    },
+    {
+        featureType: 'administrative.country',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9e9e9e' }]
+    },
+    {
+        featureType: 'administrative.land_parcel',
+        stylers: [{ visibility: 'off' }]
+    },
+    {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#bdbdbd' }]
+    },
+    {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#757575' }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#181818' }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#616161' }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#1b1b1b' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry.fill',
+        stylers: [{ color: '#2c2c2c' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#8a8a8a' }]
+    },
+    {
+        featureType: 'road.arterial',
+        elementType: 'geometry',
+        stylers: [{ color: '#373737' }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#3c3c3c' }]
+    },
+    {
+        featureType: 'road.highway.controlled_access',
+        elementType: 'geometry',
+        stylers: [{ color: '#4e4e4e' }]
+    },
+    {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#616161' }]
+    },
+    {
+        featureType: 'transit',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#757575' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#000000' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#3d3d3d' }]
+    }
+];
+
 function initMap() {
 	function initializeMapWithRandomLocation() {
 		randomLocation = getRandomLocation();
@@ -233,6 +336,7 @@ function initMap() {
 					streetViewControl: false,
 					mapTypeControl: false,
 					clickableIcons: false,
+                    styles: darkModeStyle
 				});
 
 				google.maps.event.addListener(minimap, "click", function (event) {
@@ -480,7 +584,11 @@ function returnToMainMenu() {
 	document.getElementById("result-modal").style.display = "none";
     document.getElementById("gamemap").style.display = "none";
 
-	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution: "� OpenStreetMap contributors" }).addTo(map2);
+	L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+        attribution: '© OpenStreetMap contributors © CartoDB',
+        subdomains: 'abcd',
+        maxZoom: 19
+    }).addTo(map2);
 	pauseTimer();
 }
 
