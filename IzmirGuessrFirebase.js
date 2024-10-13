ae741c5aad65e446603d292370dbe7f0775f0f94
Victiniiiii <victinii494@gmail.com>
2024-10-13 19:58:58 +0300

    bugfix

--- IzmirGuessrFirebase.js
+++ IzmirGuessrFirebase.js
@@ -1,28 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth, signInAnonymously, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyCZF3dZgi6s9-rld7alzjlqw8fTOo7mW0g",
-authDomain: "izmirguessrcompetitive.firebaseapp.com",
-projectId: "izmirguessrcompetitive",
-storageBucket: "izmirguessrcompetitive.appspot.com",
-messagingSenderId: "139244678550",
-appId: "1:139244678550:web:edbb8fa379e11439c801b7",
-measurementId: "G-345W0ZZRY9"
+    apiKey: "AIzaSyCZF3dZgi6s9-rld7alzjlqw8fTOo7mW0g",
+    authDomain: "izmirguessrcompetitive.firebaseapp.com",
+    projectId: "izmirguessrcompetitive",
+    storageBucket: "izmirguessrcompetitive.appspot.com",
+    messagingSenderId: "139244678550",
+    appId: "1:139244678550:web:edbb8fa379e11439c801b7",
+    measurementId: "G-345W0ZZRY9"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = firebase.auth();
+const auth = getAuth(app); // Use getAuth to get the auth instance
 
 // Anonymous Login Function
 function loginAnonymously() {
-    auth.signInAnonymously()
+    signInAnonymously(auth)
     .then((userCredential) => {
         const user = userCredential.user;
         console.log("Logged in anonymously:", user);
@@ -35,9 +34,9 @@ function loginAnonymously() {
 
 // Google Login Function
 function loginWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const provider = new GoogleAuthProvider(); // Use GoogleAuthProvider from the imports
 
-    auth.signInWithPopup(provider)
+    signInWithPopup(auth, provider) // Use signInWithPopup with the auth instance
     .then((result) => {
         const user = result.user;
         console.log("Logged in with Google:", user);
@@ -48,7 +47,8 @@ function loginWithGoogle() {
     });
 }
 
-auth.onAuthStateChanged((user) => {
+// Monitor Auth State
+onAuthStateChanged(auth, (user) => {
     if (user) {
         // User is signed in, you can use user.uid to identify the user
         console.log("User ID:", user.uid);
