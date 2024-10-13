abdf1eb124be875068d0d3ba841f7cdea3eab1e0
Victiniiiii <victinii494@gmail.com>
2024-10-13 19:56:00 +0300

    login functions

--- IzmirGuessrFirebase.js
+++ IzmirGuessrFirebase.js
@@ -16,25 +16,44 @@ appId: "1:139244678550:web:edbb8fa379e11439c801b7",
 measurementId: "G-345W0ZZRY9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initialize Auth
 const auth = firebase.auth();
 
-// Sign in anonymously
-auth.signInAnonymously()
-.catch((error) => {
-    console.error("Error signing in anonymously:", error);
-});
+// Anonymous Login Function
+function loginAnonymously() {
+    auth.signInAnonymously()
+    .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("Logged in anonymously:", user);
+        // You can also save user data or track gameplay here
+    })
+    .catch((error) => {
+        console.error("Error during anonymous login:", error);
+    });
+}
+
+// Google Login Function
+function loginWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider)
+    .then((result) => {
+        const user = result.user;
+        console.log("Logged in with Google:", user);
+        // You can also save user data or track gameplay here
+    })
+    .catch((error) => {
+        console.error("Error during Google login:", error);
+    });
+}
 
 auth.onAuthStateChanged((user) => {
-if (user) {
-    const uid = user.uid;
-    // You can use uid to identify the user
-    console.log("User ID:", uid);
-} else {
-    // User is signed out
-}
+    if (user) {
+        // User is signed in, you can use user.uid to identify the user
+        console.log("User ID:", user.uid);
+    } else {
+        // User is signed out
+        console.log("No user signed in.");
+    }
 });
