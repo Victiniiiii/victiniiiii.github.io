b40049fb7a507eb16b6c0fa25c9634b2b2ea912e
Victiniiiii <victinii494@gmail.com>
2024-10-14 19:33:28 +0300

    bugfix

--- IzmirGuessrFirebase.js
+++ IzmirGuessrFirebase.js
@@ -30,86 +30,96 @@ window.loginWithGoogle = function () {
 };
 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-        if (user.isAnonymous) {
-            window.document.getElementById("usernameHere").innerText = `Anonymous`;
-        } else {
-            window.document.getElementById("usernameHere").innerText = `Username: ${user.displayName}`;
-        }
-    } else {
-        signInAnonymously(auth)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log("Logged in anonymously:", user);
-        })
-        .catch((error) => {
-            console.error("Error during anonymous login:", error);
-        });
-    }
+	if (user) {
+		if (user.isAnonymous) {
+			window.document.getElementById("usernameHere").innerText = `Anonymous`;
+		} else {
+			window.document.getElementById("usernameHere").innerText = `Username: ${user.displayName}`;
+		}
+	} else {
+		signInAnonymously(auth)
+			.then((userCredential) => {
+				const user = userCredential.user;
+				console.log("Logged in anonymously:", user);
+			})
+			.catch((error) => {
+				console.error("Error during anonymous login:", error);
+			});
+	}
 });
 
 function incrementPlayCount(district) {
-    const currentUser = auth.currentUser;
-    if (currentUser && !currentUser.isAnonymous) {
-        const userId = currentUser.uid;
-        const userPlayCountsRef = doc(db, `users/${userId}/playCounts/${district}`);
-        
-        getDoc(userPlayCountsRef).then((docSnapshot) => {
-            const currentCount = docSnapshot.exists() ? docSnapshot.data().count : 0;
-            setDoc(userPlayCountsRef, { count: currentCount + 1 }, { merge: true })
-                .then(() => {
-                    console.log(`Play count for ${district} incremented successfully!`);
-                })
-                .catch((error) => {
-                    console.error(`Error incrementing play count for ${district}:`, error);
-                });
-        });
-    }
+	const currentUser = auth.currentUser;
+	if (currentUser && !currentUser.isAnonymous) {
+		const userId = currentUser.uid;
+		const userPlayCountsRef = doc(db, `users/${userId}/playCounts/${district}`);
+
+		getDoc(userPlayCountsRef).then((docSnapshot) => {
+			const currentCount = docSnapshot.exists() ? docSnapshot.data().count : 0;
+			setDoc(userPlayCountsRef, { count: currentCount + 1 }, { merge: true })
+				.then(() => {
+					console.log(`Play count for ${district} incremented successfully!`);
+				})
+				.catch((error) => {
+					console.error(`Error incrementing play count for ${district}:`, error);
+				});
+		});
+	}
 }
 
 function updateHighScore(district, score) {
-    const currentUser = auth.currentUser;
-    if (currentUser && !currentUser.isAnonymous) {
-        const userId = auth.currentUser.uid;
-        const userHighScore = doc(db, `users/${userId}/HighScores/${district}`);
+	const currentUser = auth.currentUser;
+	if (currentUser && !currentUser.isAnonymous) {
+		const userId = auth.currentUser.uid;
+		const userHighScore = doc(db, `users/${userId}/HighScores/${district}`);
 
-        getDoc(userHighScore).then((docSnapshot) => {
-            const currentCount = docSnapshot.exists() ? docSnapshot.data().count : 0;
-            if (!docSnapshot.exists() || currentCount < score) {
-                setDoc(userHighScore, { count: score }, { merge: true })
-                    .then(() => {
-                        console.log(`Current High Score for ${district}:`, currentCount);
-                        console.log(`New Score Attempt:`, score);
-                        console.log(`High Score for ${district} updated successfully!`);
-                    })
-                    .catch((error) => {
-                        console.error(`Error updating high score for ${district}:`, error);
-                    });
-            } else {
-                console.log("High score not high enough");
-            }
-        }).catch((error) => {
-            console.error(`Error retrieving high score for ${district}:`, error);
-        });
-    }
+		getDoc(userHighScore)
+			.then((docSnapshot) => {
+				const currentCount = docSnapshot.exists() ? docSnapshot.data().count : 0;
+				if (!docSnapshot.exists() || currentCount < score) {
+					setDoc(userHighScore, { count: score }, { merge: true })
+						.then(() => {
+							console.log(`Current High Score for ${district}:`, currentCount);
+							console.log(`New Score Attempt:`, score);
+							console.log(`High Score for ${district} updated successfully!`);
+						})
+						.catch((error) => {
+							console.error(`Error updating high score for ${district}:`, error);
+						});
+				} else {
+					console.log("High score not high enough");
+				}
+			})
+			.catch((error) => {
+				console.error(`Error retrieving high score for ${district}:`, error);
+			});
+	}
 }
 
 async function increaseRoundCount(district) {
-    const Ref = doc(db, `users/${userId}/${district}`);
+	const currentUser = auth.currentUser;
+	if (currentUser && !currentUser.isAnonymous) {
+		const userId = auth.currentUser.uid;
+		const Ref = doc(db, `users/${userId}/${district}`);
 
-    await updateDoc(Ref, {
-        playCount: increment(1)
-    });
-    console.log(`Incremented by 1: ${district}`);
+		await updateDoc(Ref, {
+			playCount: increment(1),
+		});
+		console.log(`Incremented by 1: ${district}`);
+	}
 }
 
-async function addToTotalScore(district,number) {
-    const Ref = doc(db, `users/${userId}/${district}`);
+async function addToTotalScore(district, number) {
+	const currentUser = auth.currentUser;
+	if (currentUser && !currentUser.isAnonymous) {
+		const userId = auth.currentUser.uid;
+		const Ref = doc(db, `users/${userId}/${district}`);
 
-    await updateDoc(Ref, {
-        TotalScore: increment(number)
-    });
-    console.log(`Total score for ${district} increased by ${number}`)
+		await updateDoc(Ref, {
+			TotalScore: increment(number),
+		});
+		console.log(`Total score for ${district} increased by ${number}`);
+	}
 }
 
 // game count, high score, round count, total score, hepsi-tek ilçe ayrımına dikkat
