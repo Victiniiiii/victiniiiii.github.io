3007a929cca71f12b8ac4da0850faae0fe0e2f48
Victiniiiii <victinii494@gmail.com>
2024-10-17 13:29:07 +0300

    changed the formatting of the zoom

--- IzmirGuessrCoordinates.js
+++ IzmirGuessrCoordinates.js
@@ -2,7 +2,7 @@ const districtsData = [
 	{
 		name: "Çeşme",
 		state: 1,
-        zoom: [38.3217, 26.3030],
+        zoom: { lat: 38.3217, lng: 26.3030 },
 		bounds: [
 			[38.264265, 26.500253],
 			[38.266152, 26.470427],
@@ -463,7 +463,7 @@ const districtsData = [
 	{
 		name: "Urla",
 		state: 1,
-        zoom: [38.3219, 26.7665],
+        zoom: { lat: 38.3219, lng: 26.7665 },
 		bounds: [
 			[38.374122, 26.854488],
 			[38.36921, 26.856461],
@@ -866,7 +866,7 @@ const districtsData = [
 	{
 		name: "Karaburun",
 		state: 1,
-        zoom: [38.6357, 26.5083],
+        zoom: { lat: 38.6357, lng: 26.5083 },
 		bounds: [
 			[38.442548, 26.600101],
 			[38.441876, 26.593327],
@@ -948,7 +948,7 @@ const districtsData = [
 	{
 		name: "Seferihisar",
 		state: 1,
-        zoom: [38.1941, 26.8379],
+        zoom: { lat: 38.1941, lng: 26.8379 },
 		bounds: [
 			[38.263372, 26.832164],
 			[38.235736, 26.827105],
@@ -1005,7 +1005,7 @@ const districtsData = [
 	{
 		name: "Güzelbahçe",
 		state: 1,
-        zoom: [38.3559, 26.8863],
+        zoom: { lat: 38.3559, lng: 26.8863 },
 		bounds: [
 			[38.373531, 26.854939],
 			[38.369218, 26.857913],
@@ -1043,7 +1043,7 @@ const districtsData = [
 	{
 		name: "Menderes",
 		state: 1,
-        zoom: [38.2502, 27.1342],
+        zoom: { lat: 38.2502, lng: 27.1342 },
 		bounds: [
 			[38.290519, 27.141896],
 			[38.290554, 27.146465],
@@ -1134,7 +1134,7 @@ const districtsData = [
 	{
 		name: "Narlıdere",
 		state: 1,
-        zoom: [38.3933, 27.0095],
+        zoom: { lat: 38.3933, lng: 27.0095 },
 		bounds: [
 			[38.413272, 27.015175],
 			[38.412935, 27.017288],
@@ -1193,7 +1193,7 @@ const districtsData = [
 	{
 		name: "Balçova",
 		state: 1,
-        zoom: [38.3917, 27.0428],
+        zoom: { lat: 38.3917, lng: 27.0428 },
 		bounds: [
 			[38.413322, 27.015238],
 			[38.412994, 27.017392],
@@ -1296,7 +1296,7 @@ const districtsData = [
 	{
 		name: "Selçuk",
 		state: 1,
-        zoom: [37.9479, 27.3697],
+        zoom: { lat: 37.9479, lng: 27.3697 },
 		bounds: [
 			[38.035494, 27.441592],
 			[38.036035, 27.44331],
@@ -1370,7 +1370,7 @@ const districtsData = [
 	{
 		name: "Torbalı",
 		state: 1,
-        zoom: [38.1513, 27.3621],
+        zoom: { lat: 38.1513, lng: 27.3621 },
 		bounds: [
 			[38.08118, 27.401827],
 			[38.080707, 27.400452],
@@ -1447,7 +1447,7 @@ const districtsData = [
 	{
 		name: "Tire",
 		state: 1,
-        zoom: [38.0854, 27.7350],
+        zoom: { lat: 38.0854, lng: 27.7350 },
 		bounds: [
 			[38.108439, 27.730908],
 			[38.088445, 27.754949],
@@ -1503,7 +1503,7 @@ const districtsData = [
 	{
 		name: "Ödemiş",
 		state: 1,
-        zoom: [38.2259, 27.9691],
+        zoom: { lat: 38.2259, lng: 27.9691 },
 		bounds: [
 			[38.272864, 28.002038],
 			[38.248063, 27.985548],
@@ -1559,7 +1559,7 @@ const districtsData = [
 	{
 		name: "Bayındır",
 		state: 1,
-        zoom: [38.2178, 27.6457],
+        zoom: { lat: 38.2178, lng: 27.6457 },
 		bounds: [
 			[38.235167, 27.647054],
 			[38.22357, 27.633827],
@@ -1606,7 +1606,7 @@ const districtsData = [
 	{
 		name: "Kiraz",
 		state: 1,
-        zoom: [38.2304, 28.2050],
+        zoom: { lat: 38.2304, lng: 28.2050 },
 		bounds: [
 			[38.242023, 28.176894],
 			[38.246067, 28.219106],
@@ -1639,7 +1639,7 @@ const districtsData = [
 	{
 		name: "Beydağ",
 		state: 1,
-        zoom: [38.0858, 28.2086],
+        zoom: { lat: 38.0858, lng: 28.2086 },
 		bounds: [
 			[38.104677, 28.218584],
 			[38.099138, 28.222872],
@@ -1670,7 +1670,7 @@ const districtsData = [
 	{
 		name: "Kemalpaşa",
 		state: 1,
-        zoom: [38.4270, 27.4167],
+        zoom: { lat: 38.4270, lng: 27.4167 },
 		bounds: [
 			[38.457479, 27.325848],
 			[38.4674, 27.323038],
@@ -1733,7 +1733,7 @@ const districtsData = [
 	{
 		name: "Konak",
 		state: 1,
-        zoom: [38.4179, 27.1283],
+        zoom: { lat: 38.4179, lng: 27.1283 },
 		bounds: [
 			[38.442657, 27.142617],
 			[38.447127, 27.148576],
@@ -1913,7 +1913,7 @@ const districtsData = [
 	{
 		name: "Buca",
 		state: 1,
-        zoom: [38.3871, 27.1743],
+        zoom: { lat: 38.3871, lng: 27.1743 },
 		bounds: [
 			[38.394343, 27.138385],
 			[38.392174, 27.138069],
@@ -2045,7 +2045,7 @@ const districtsData = [
 	{
 		name: "Gaziemir",
 		state: 1,
-        zoom: [38.3198, 27.1318],
+        zoom: { lat: 38.3198, lng: 27.1318 },
 		bounds: [
 			[38.347799, 27.10441],
 			[38.354059, 27.116164],
@@ -2117,7 +2117,7 @@ const districtsData = [
 	{
 		name: "Karabağlar",
 		state: 1,
-        zoom: [38.3949, 27.1283],
+        zoom: { lat: 38.3949, lng: 27.1283 },
 		bounds: [
 			[38.396816, 27.064235],
 			[38.396295, 27.064235],
@@ -2284,7 +2284,7 @@ const districtsData = [
 	{
 		name: "Bornova",
 		state: 1,
-        zoom: [38.4655, 27.2195],
+        zoom: { lat: 38.4655, lng: 27.2195 },
 		bounds: [
 			[38.492958, 27.186516],
 			[38.487684, 27.1812],
@@ -2417,7 +2417,7 @@ const districtsData = [
 	{
 		name: "Menemen",
 		state: 1,
-        zoom: [38.6096, 27.0667],
+        zoom: { lat: 38.6096, lng: 27.0667 },
 		bounds: [
 			[38.631874, 26.909809],
 			[38.630868, 26.913325],
@@ -2614,7 +2614,7 @@ const districtsData = [
 	{
 		name: "Aliağa",
 		state: 1,
-        zoom: [38.8017, 26.9733],
+        zoom: { lat: 38.8017, lng: 26.9733 },
 		bounds: [
 			[38.937564, 27.054931],
 			[38.935895, 27.051587],
@@ -2857,7 +2857,7 @@ const districtsData = [
 	{
 		name: "Bergama",
 		state: 1,
-        zoom: [39.1188, 27.1772],
+        zoom: { lat: 39.1188, lng: 27.1772 },
 		bounds: [
 			[39.114046, 27.255502],
 			[39.120172, 27.224634],
@@ -2940,7 +2940,7 @@ const districtsData = [
 	{
 		name: "Kınık",
 		state: 1,
-        zoom: [39.0872, 27.3827],
+        zoom: { lat: 39.0872, lng: 27.3827 },
 		bounds: [
 			[39.143841, 27.468648],
 			[39.126798, 27.457118],
@@ -2992,7 +2992,7 @@ const districtsData = [
 	{
 		name: "Dikili",
 		state: 1,
-        zoom: [39.0742, 26.8898],
+        zoom: { lat: 39.0742, lng: 26.8898 },
 		bounds: [
 			[39.213469, 26.821013],
 			[39.18713, 26.791511],
@@ -3073,7 +3073,7 @@ const districtsData = [
 	{
 		name: "Foça",
 		state: 1,
-        zoom: [38.6683, 26.7549],
+        zoom: { lat: 38.6683, lng: 26.7549 },
 		bounds: [
 			[38.669412, 26.749542],
 			[38.677051, 26.741996],
@@ -3224,7 +3224,7 @@ const districtsData = [
 	{
 		name: "Karşıyaka",
 		state: 1,
-        zoom: [38.4555, 27.1194],
+        zoom: { lat: 38.4555, lng: 27.1194 },
 		bounds: [
 			[38.4752, 27.0643],
 			[38.4745, 27.0626],
@@ -3327,7 +3327,7 @@ const districtsData = [
 	{
 		name: "Bayraklı",
 		state: 1,
-        zoom: [38.4641, 27.1680],
+        zoom: { lat: 38.4641, lng: 27.1680 },
 		bounds: [
 			[38.464254, 27.148053],
 			[38.464809, 27.145052],
@@ -3406,7 +3406,7 @@ const districtsData = [
 	{
 		name: "Çiğli",
 		state: 1,
-        zoom: [38.4936, 27.0627],
+        zoom: { lat: 38.4936, lng: 27.0627 },
 		bounds: [
 			[38.519744, 27.039601],
 			[38.523135, 27.041359],
