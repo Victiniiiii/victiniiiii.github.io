6c290795c7426fe8e2bbd45d3f098f209899b745
Victiniiiii <victinii494@gmail.com>
2024-10-18 19:30:22 +0300

    oooooops

--- IzmirGuessrFirebase.js
+++ IzmirGuessrFirebase.js
@@ -64,7 +64,7 @@ thirdButton.addEventListener('click', () => {
 function changeNickname() {
     runTransaction(db, async (transaction) => {
         const currentUser = auth.currentUser;
-        const ref = doc(db, `users/${currentUser.uid}/UserData/Nickname}`);
+        const ref = doc(db, `users/${currentUser.uid}/UserData/Nickname`);
         const userData = await transaction.get(ref);
         const input = document.getElementById("changeUsernameInput").value;
         console.log("input:",input);
