3dfad60e5052b907f12a689278046ca057afaef1
Victiniiiii <victinii494@gmail.com>
2024-11-14 20:34:24 +0300

    preventing the service worker popping on other pages

--- ortalamatorSW.js
+++ ortalamatorSW.js
@@ -1,22 +1,17 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('subpage-cache-v1').then((cache) => {
-        return cache.addAll([
-          '/ortalamator.html',
-          '/static/ortalamatorPWAstyles.css',
-          '/ortalamator.js',
-          '/static/images/ortalamator192.png',
-          '/static/images/ortalamator512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+if (document.title == "Ortalamatör") {
+	self.addEventListener("install", (event) => {
+		event.waitUntil(
+			caches.open("subpage-cache-v1").then((cache) => {
+				return cache.addAll(["/ortalamator.html", "/static/ortalamatorPWAstyles.css", "/ortalamator.js", "/static/images/ortalamator192.png", "/static/images/ortalamator512.png"]);
+			})
+		);
+	});
+
+	self.addEventListener("fetch", (event) => {
+		event.respondWith(
+			caches.match(event.request).then((response) => {
+				return response || fetch(event.request);
+			})
+		);
+	});
+}
