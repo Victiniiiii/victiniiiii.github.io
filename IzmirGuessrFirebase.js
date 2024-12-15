4613a6bb19c278a82e758049c12a95bd818526ae
Victiniiiii <victinii494@gmail.com>
2024-12-15 16:29:13 +0300

    some fixes and changes

--- IzmirGuessrFirebase.js
+++ IzmirGuessrFirebase.js
@@ -369,8 +369,12 @@ async function loadMatchHistory() {
 			documents.sort((a, b) => b.id.localeCompare(a.id, "tr"));
 
 			documents.forEach((doc) => {
-				const data = doc.data;
-				modalMatchHistory.innerHTML += `<p> Date: ${data.date}, Game Mode: ${data.gameMode}, Score: ${data.score}, Round Times: ${data.time}, Coordinates: ${data.coordinates.lat} ${data.coordinates.lng}</p>`;
+				const data = doc.data();
+                const totalScore = data.score.reduce((acc, score) => acc + score, 0);
+				modalMatchHistory.innerHTML += `<br> <p> Date: ${data.date}, Game Mode: ${data.gameMode}, Score: ${totalScore} </p> <br>`;
+                for ( i = 0; i < data.score.length; i++) {
+                    modalMatchHistory.innerHTML += `<p> Round ${i + 1} --> Score: ${data.score[i]}, Time: ${data.time[i]}, Coordinates: ${data.coordinates[i].lat}, ${data.coordinates[i].lng} </p>`;
+                }
 			});
 		}
 	} else {
