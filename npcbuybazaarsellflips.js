b48af5f1315553e1b5011c3ec0fe77eb2c385792
Victiniiiii <victinii494@gmail.com>
2025-02-18 03:11:37 +0300

    the items are now sorted on profit

--- npcbuybazaarsellflips.js
+++ npcbuybazaarsellflips.js
@@ -7,7 +7,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Lumber Merchant",
 		npc: "5",
-        image: "ACACIA_WOOD"
+		image: "ACACIA_WOOD",
 	},
 	{
 		name: "Carrot",
@@ -15,7 +15,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Farm Merchant",
 		npc: "10",
-        image: "CARROT"
+		image: "CARROT",
 	},
 	{
 		name: "Cocoa Beans",
@@ -23,7 +23,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Farm Merchant",
 		npc: "5",
-        image: "COCOA_BEANS"
+		image: "COCOA_BEANS",
 	},
 	{
 		name: "Coal",
@@ -52,7 +52,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Librarian",
 		npc: "30",
-        image: "EXPERIENCE_BOTTLE"
+		image: "EXPERIENCE_BOTTLE",
 	},
 	{
 		name: "Flint",
@@ -67,7 +67,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Mage Emissary",
 		npc: "5000",
-        image: "EXPERIENCE_BOTTLE"
+		image: "EXPERIENCE_BOTTLE",
 	},
 	{
 		name: "Gunpowder",
@@ -75,7 +75,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Adventurer",
 		npc: "10",
-        image: "GUNPOWDER"
+		image: "GUNPOWDER",
 	},
 	{
 		name: "Ice",
@@ -97,7 +97,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Lumber Merchant",
 		npc: "5",
-        image: "JUNGLE_WOOD"
+		image: "JUNGLE_WOOD",
 	},
 	{
 		name: "Magma Cream",
@@ -119,7 +119,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Lumber Merchant",
 		npc: "5",
-        image: "OAK_WOOD"
+		image: "OAK_WOOD",
 	},
 	{
 		name: "Packed Ice",
@@ -134,7 +134,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Fish Merchant",
 		npc: "40",
-        image: "PUFFERFISH"
+		image: "PUFFERFISH",
 	},
 	{
 		name: "Potato",
@@ -142,7 +142,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Farm Merchant",
 		npc: "10",
-        image: "POTATO"
+		image: "POTATO",
 	},
 	{
 		name: "Pumpkin",
@@ -171,7 +171,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Adventurer",
 		npc: "14",
-        image: "SLIMEBALL"
+		image: "SLIMEBALL",
 	},
 	{
 		name: "Spruce Wood",
@@ -179,7 +179,7 @@ let itemsarray = [
 		amount: "640",
 		source: "Lumber Merchant",
 		npc: "5",
-        image: "SPRUCE_WOOD"
+		image: "SPRUCE_WOOD",
 	},
 	{
 		name: "String",
@@ -202,159 +202,159 @@ let itemsarray = [
 		source: "Farm Merchant",
 		npc: "10",
 	},
-    {
+	{
 		name: "Rotten Flesh",
 		id: "ROTTEN_FLESH",
 		amount: "640",
 		source: "Adventurer",
 		npc: "8",
 	},
-    {
+	{
 		name: "Bone",
 		id: "BONE",
 		amount: "640",
 		source: "Adventurer",
 		npc: "8",
 	},
-    {
+	{
 		name: "Birch Wood",
 		id: "LOG:2",
 		amount: "640",
 		source: "Lumber Merchant",
 		npc: "5",
-        image: "BIRCH_WOOD"
+		image: "BIRCH_WOOD",
 	},
-    {
+	{
 		name: "Dark Oak Wood",
 		id: "LOG_2:1",
 		amount: "640",
 		source: "Lumber Merchant",
 		npc: "5",
-        image: "DARK_OAK_WOOD"
+		image: "DARK_OAK_WOOD",
 	},
-    {
+	{
 		name: "Raw Salmon",
 		id: "RAW_FISH:1",
 		amount: "640",
 		source: "Fish Merchant",
 		npc: "30",
-        image: "RAW_SALMON"
+		image: "RAW_SALMON",
 	},
-    {
+	{
 		name: "Clownfish",
 		id: "RAW_FISH:2",
 		amount: "640",
 		source: "Fish Merchant",
 		npc: "100",
-        image: "CLOWNFISH"
+		image: "CLOWNFISH",
 	},
-    {
+	{
 		name: "Gold Ingot",
 		id: "GOLD_INGOT",
 		amount: "1280",
 		source: "Mine Merchant, Gold Forger",
 		npc: "5.75",
 	},
-    {
+	{
 		name: "Gravel",
 		id: "GRAVEL",
 		amount: "1920",
 		source: "Mine Merchant, Builder, Pat",
 		npc: "4.33",
 	},
-    {
+	{
 		name: "Red Mushroom",
 		id: "RED_MUSHROOM",
 		amount: "640",
 		source: "Farm Merchant",
 		npc: "12",
 	},
-    {
+	{
 		name: "Brown Mushroom",
 		id: "BROWN_MUSHROOM",
 		amount: "640",
 		source: "Farm Merchant",
 		npc: "12",
 	},
-    {
+	{
 		name: "Cactus",
 		id: "CACTUS",
 		amount: "640",
 		source: "Farm Merchant",
 		npc: "3",
 	},
-    {
+	{
 		name: "White Wool",
 		id: "WOOL",
 		amount: "640",
 		source: "Wool Weaver",
 		npc: "32",
-        image: "WHITE_WOOL"
+		image: "WHITE_WOOL",
 	},
-    {
+	{
 		name: "Netherrack",
 		id: "NETHERRACK",
 		amount: "640",
 		source: "Builder",
 		npc: "2",
 	},
-    {
+	{
 		name: "Obsidian",
 		id: "OBSIDIAN",
 		amount: "640",
 		source: "Pearl Dealer",
 		npc: "50",
 	},
-    {
+	{
 		name: "Nether Wart",
 		id: "NETHER_STALK",
 		amount: "640",
 		source: "Alchemist",
 		npc: "10",
-        image: "NETHER_WART"
+		image: "NETHER_WART",
 	},
-    {
+	{
 		name: "Ghast Tear",
 		id: "GHAST_TEAR",
 		amount: "640",
 		source: "Alchemist",
 		npc: "200",
 	},
-    {
+	{
 		name: "Rabbit's Foot",
 		id: "RABBIT_FOOT",
 		amount: "640",
 		source: "Alchemist",
 		npc: "10",
 	},
-    {
+	{
 		name: "Spider Eye",
 		id: "SPIDER_EYE",
 		amount: "640",
 		source: "Alchemist",
 		npc: "12",
 	},
-    {
+	{
 		name: "Redstone",
 		id: "REDSTONE",
 		amount: "640",
 		source: "Mad Redstone Engineer",
 		npc: "4",
 	},
-    {
+	{
 		name: "Superboom TNT",
 		id: "SUPERBOOM_TNT",
 		amount: "1280",
 		source: "Walter, Ophelia",
 		npc: "2750",
-        image: "ENCHANTED_TNT"
-	}
+		image: "ENCHANTED_TNT",
+	},
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
 	var webptest = new Image(1, 1);
 	webptest.src = "/static/imageswebp/webpdot.webp";
-	webptest.width > 0 && webptest.height > 0 ? WebPSupport = 1 : WebPSupport = 0;	
+	webptest.width > 0 && webptest.height > 0 ? (WebPSupport = 1) : (WebPSupport = 0);
 
 	const savedToggleStates = localStorage.getItem("toggleStates");
 	if (savedToggleStates) {
@@ -364,9 +364,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 	document.getElementById("settings-bar").addEventListener("click", function () {
-        document.getElementById("settings").classList.toggle("expanded");
-      });
-      
+		document.getElementById("settings").classList.toggle("expanded");
+	});
 
 	const container = document.getElementById("toggle-container");
 
@@ -452,10 +451,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 		const nbbsImage = document.createElement("img");
 		if (WebPSupport == 1) {
-            nbbsImage.src = `static/imageswebp/${itemsarray[i].image ? itemsarray[i].image : itemsarray[i].id}.webp`;
-        } else {
-            nbbsImage.src = `static/images/${itemsarray[i].image ? itemsarray[i].image : itemsarray[i].id}.png`;
-        }        
+			nbbsImage.src = `static/imageswebp/${itemsarray[i].image ? itemsarray[i].image : itemsarray[i].id}.webp`;
+		} else {
+			nbbsImage.src = `static/images/${itemsarray[i].image ? itemsarray[i].image : itemsarray[i].id}.png`;
+		}
 
 		const theBox = document.createElement("div");
 
@@ -470,23 +469,40 @@ async function bazaarconnect() {
 	const response = await fetch("https://api.hypixel.net/v2/skyblock/bazaar");
 	const data = await response.json();
 
+	let boxesWithProfit = [];
+
 	for (let i = 0; i < itemsarray.length; i++) {
-        console.log(format(data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"].toFixed(0)) + " coins");
-		document.getElementById(`prices${itemsarray[i].id}`).innerHTML = format(data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"].toFixed(0)) + " coins";
-		document.getElementById(`NbBsBox${i}`).innerHTML = itemsarray[i].name + "<br>";
-		document.getElementById(`NbBsBox${i}`).innerHTML += "Profit: ";
-		document.getElementById(`NbBsBox${i}`).innerHTML += format((itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc)).toFixed(0)) + " coins<br>";
-		if (itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc) > 100000) {
-			document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#0f6319";
-		} else if (itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc) > 10000) {
-			document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#81cc8a";
-		} else if (itemsarray[i].amount * (data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] - itemsarray[i].npc) > 0) {
-			document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#d5db23";
+		let price = data.products[itemsarray[i].id]?.quick_status[toggleStates[i] ? "buyPrice" : "sellPrice"] || 0;
+		let profit = (itemsarray[i].amount * (price - itemsarray[i].npc)).toFixed(0);
+
+		const box = document.getElementById(`NbBsBox${i}`);
+		box.innerHTML = `${itemsarray[i].name}<br>`;
+		box.innerHTML += `Profit: ${format(profit)} coins<br>`;
+		box.innerHTML += `Source: ${itemsarray[i].source}<br>`;
+
+		document.getElementById(`prices${itemsarray[i].id}`).innerHTML = format(price.toFixed(0)) + " coins";
+
+		if (profit > 100000) {
+			box.style.backgroundColor = "#0f6319";
+		} else if (profit > 10000) {
+			box.style.backgroundColor = "#81cc8a";
+		} else if (profit > 0) {
+			box.style.backgroundColor = "#d5db23";
 		} else {
-			document.getElementById(`NbBsBox${i}`).style.backgroundColor = "#db2323";
+			box.style.backgroundColor = "#db2323";
 		}
-		document.getElementById(`NbBsBox${i}`).innerHTML += `Source: ${itemsarray[i].source}`;
+
+		if (document.getElementById(`settings${i}`)) {
+			box.innerHTML += document.getElementById(`settings${i}`).outerHTML;
+		}
+
+		boxesWithProfit.push({ box: box.parentElement, profit: parseFloat(profit) });
 	}
+
+	boxesWithProfit.sort((a, b) => b.profit - a.profit);
+
+	const NbBsSpace = document.getElementById("NbBsSpace");
+	boxesWithProfit.forEach((item) => NbBsSpace.appendChild(item.box));
 }
 
 function format(x) {
