d99b60e3c5b37dc44cd0c7a3424dd689c5adc92b
Victiniiiii <victinii494@gmail.com>
2025-03-12 14:59:05 +0300

    almost forgot

--- IzmirGuessrCompetitive.js
+++ IzmirGuessrCompetitive.js
@@ -1193,14 +1193,14 @@ document.querySelectorAll(".firebaseButton").forEach((button) => {
 	});
 });
 
-/* document.addEventListener("contextmenu", function (event) {
+document.addEventListener("contextmenu", function (event) {
 	event.preventDefault();
 	if (!gameOngoing && !initiallyGreenDistricts.length == 0) {
 		removeAllDistricts();
 	} else if (!gameOngoing && initiallyGreenDistricts.length == 0) {
 		addAllDistricts();
 	}
-}); */
+});
 
 let marker = new Image();
 marker.src = "static/images/redpin.png";
