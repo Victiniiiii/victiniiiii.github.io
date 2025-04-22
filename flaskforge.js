67c27e36b0559530e5dd544324d6add7199939e0
Victiniiiii <victinii494@gmail.com>
2025-04-22 17:02:41 +0300

    Revert "ok im reverting it back"
    This reverts commit c005b88b46189c07993f8493aeccb4a0342dacd7.

---
+++ flaskforge.js
@@ -0,0 +1,56 @@
+const gemstoneNames = ["JADE_GEM", "AMBER_GEM", "TOPAZ_GEM", "SAPPHIRE_GEM", "AMETHYST_GEM", "RUBY_GEM", "JASPER_GEM", "OPAL_GEM", "ONYX_GEM", "AQUAMARINE_GEM", "CITRINE_GEM", "PERIDOT_GEM"];
+const capitalizedgemstoneNames = ["Jade", "Amber", "Topaz", "Sapphire", "Amethyst", "Ruby", "Jasper", "Opal", "Onyx", "Aquamarine", "Citrine", "Peridot"];
+
+async function gemstonenames() {
+	const response = await fetch("https://api.hypixel.net/skyblock/bazaar");
+	const data = await response.json();
+
+	const getGemstoneInfo = (gemName) => {
+		const fineGem = data.products[`FINE_${gemName}`]?.quick_status;
+		const flawlessGem = data.products[`FLAWLESS_${gemName}`]?.quick_status;
+		const perfectGem = data.products[`PERFECT_${gemName}`]?.quick_status;
+
+		if (!fineGem || !flawlessGem || !perfectGem) {
+			console.error(`Gem data not found for ${gemName}`);
+			return {
+				fineSentence: "",
+				flawlessSentence: "",
+			};
+		}
+
+		const fineGemPrice = fineGem.sellPrice;
+		const flawlessGemPrice = flawlessGem.sellPrice;
+		const perfectGemBuyPrice = perfectGem.buyPrice;
+
+		const fineGemProfit = perfectGemBuyPrice - fineGemPrice * 400;
+		const flawlessGemProfit = perfectGemBuyPrice - flawlessGemPrice * 5;
+
+		const formatNumber = (number) => Math.floor(number).toLocaleString().replace(/,/g, ",");
+
+		const capitalizedGemName = gemName
+			.toLowerCase()
+			.split("_")
+			.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+			.join(" ");
+
+		return {
+			fineSentence: ` 400 Fine ${capitalizedGemName} Gemstones are $${formatNumber(fineGemPrice * 400)}, <br> and 1 Perfect ${capitalizedGemName} Gemstone is $${formatNumber(perfectGemBuyPrice)}.<br>The profit is $${formatNumber(fineGemProfit)}.`,
+			flawlessSentence: ` 5 Flawless ${capitalizedGemName} Gemstones are $${formatNumber(flawlessGemPrice * 5)}, <br> and 1 Perfect ${capitalizedGemName} Gemstone is $${formatNumber(perfectGemBuyPrice)}.<br>The profit is $${formatNumber(flawlessGemProfit)}.`,
+		};
+	};
+
+	const gemstoneInfoArray = gemstoneNames.map(getGemstoneInfo);
+	const gemstoneSentences = gemstoneInfoArray.flatMap(({ fineSentence, flawlessSentence }) => [fineSentence, flawlessSentence]);
+	return gemstoneSentences;
+}
+
+gemstonenames().then((sentences) => {
+	const gemstoneTexts = sentences.reduce((acc, sentence, index) => {
+		acc[`text${index + 1}`] = sentence;
+		return acc;
+	}, {});
+
+	Object.entries(gemstoneTexts).forEach(([key, value]) => {
+		document.getElementById(key).innerHTML = value;
+	});
+});
---
+++ test.js
@@ -0,0 +1,82 @@
+function isPointInPolygon(point, polygon) {
+	let x = point[0],
+		y = point[1];
+	let inside = false;
+	for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+		let xi = polygon[i][0],
+			yi = polygon[i][1];
+		let xj = polygon[j][0],
+			yj = polygon[j][1];
+		let intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+		if (intersect) inside = !inside;
+	}
+	return inside;
+}
+
+function randomPointInPolygon(polygon) {
+	let minX = polygon[0][0];
+	let maxX = polygon[0][0];
+	let minY = polygon[0][1];
+	let maxY = polygon[0][1];
+	for (let i = 1; i < polygon.length; i++) {
+		minX = Math.min(minX, polygon[i][0]);
+		maxX = Math.max(maxX, polygon[i][0]);
+		minY = Math.min(minY, polygon[i][1]);
+		maxY = Math.max(maxY, polygon[i][1]);
+	}
+	let point;
+	do {
+		point = [Math.random() * (maxX - minX) + minX, Math.random() * (maxY - minY) + minY];
+	} while (!isPointInPolygon(point, polygon));
+	return point;
+}
+
+let map = L.map("map").setView([37.955, 27.375], 10);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	maxZoom: 19,
+}).addTo(map);
+
+const districtColors = [
+    "blue",      // KaraburunBounds
+    "green",     // CesmeBounds
+    "red",       // KinikBounds
+    "purple",    // DikiliBounds
+    "yellow",    // BergamaBounds
+    "brown",     // UrlaBounds
+    "turquoise", // FocaBounds
+    "darkblue",  // AliagaBounds
+    "green",    // MenemenBounds
+    "yellow",     // CigliBounds
+    "blue",      // KarsiyakaBounds
+    "green",     // BayrakliBounds
+    "purple",    // KonakBounds
+    "black",     // BeydagBounds
+    "turquoise", // BornovaBounds
+    "black",     // GuzelbahceBounds
+    "yellow",    // KemalpasaBounds
+    "red",       // SeferihisarBounds
+    "turquoise", // BalcovaBounds
+    "orange",    // NarlidereBounds
+    "darkblue",  // KarabaglarBounds
+    "yellow",    // BucaBounds
+    "red",       // KirazBounds
+    "green",     // GaziemirBounds
+    "black",     // TireBounds
+    "blue",      // MenderesBounds
+    "brown",     // SelcukBounds
+    "pink",      // OdemisBounds
+    "purple",    // TorbaliBounds
+    "turquoise"  // BayindirBounds
+];
+
+function bounds() {
+    for (let i = 0; i < 30; i++) {
+        L.polygon(districtsData[i].bounds, { color: districtColors[i] }).addTo(map);
+    }
+}
+
+function designcoordinates() {
+    for (let i = 0; i < 30; i++) {
+        L.polygon(districtsData[i].designcoordinates, { color: districtColors[i] }).addTo(map);
+    }
+}
